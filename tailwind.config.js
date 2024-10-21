b91897bef2644585cce515dae6ce0fa0d17f9f2b
KorbiQWeidinger <korbi.weidinger2@gmail.com>
2024-10-21 14:52:32 +0200

    feat: twitter logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,44 +3,44 @@ export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-  	extend: {
-  		colors: {
-  			gold: {
-  				'500': '#DAA520'
-  			}
-  		},
-  		fontFamily: {
-  			playfair: ['Playfair Display"', "serif"],
-  			times: ["Times New Roman", "Times", "serif"]
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		keyframes: {
-  			marquee: {
-  				from: {
-  					transform: 'translateX(0)'
-  				},
-  				to: {
-  					transform: 'translateX(calc(-100% - var(--gap)))'
-  				}
-  			},
-  			'marquee-vertical': {
-  				from: {
-  					transform: 'translateY(0)'
-  				},
-  				to: {
-  					transform: 'translateY(calc(-100% - var(--gap)))'
-  				}
-  			}
-  		},
-  		animation: {
-  			marquee: 'marquee var(--duration) infinite linear',
-  			'marquee-vertical': 'marquee-vertical var(--duration) linear infinite'
-  		}
-  	}
+    extend: {
+      colors: {
+        gold: {
+          500: "#DAA520",
+        },
+      },
+      fontFamily: {
+        playfair: ['Playfair Display"', "serif"],
+        times: ["Times New Roman", "Times", "serif"],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        marquee: {
+          from: {
+            transform: "translateX(0)",
+          },
+          to: {
+            transform: "translateX(calc(-100% - var(--gap)))",
+          },
+        },
+        "marquee-vertical": {
+          from: {
+            transform: "translateY(0)",
+          },
+          to: {
+            transform: "translateY(calc(-100% - var(--gap)))",
+          },
+        },
+      },
+      animation: {
+        marquee: "marquee var(--duration) infinite linear",
+        "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
+      },
+    },
   },
   // eslint-disable-next-line no-undef
   plugins: [require("tailwindcss-animate")],
